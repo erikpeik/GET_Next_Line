c0e469310e679b7808a94ef1193970fa5ee5b6cf
Erik Mende <emende@student.hive.fi>
2022-01-11 20:29:25 +0200

    Removed comments, but not satisfied to this solution

--- get_next_line.c
+++ get_next_line.c
@@ -20,7 +20,6 @@ static int	ft_joinandfree(char **line, char **arr)
 	if (!new)
 		return (-1);
 	free(*line);
-	//free(*line);
 	//*line = NULL;
 	*line = new;
 	return (0);
@@ -38,36 +37,25 @@ static int	ft_malloc_buffer(char **arr)
 static int	ft_last_part(char **line, char **arr, char *eol)
 {
 	char	*new;
-	//long	res;
+	long	res;
 
-	//res = eol - *arr + 1;
 	*eol = '\0';
+	res = eol - *arr;
 	ft_joinandfree(line, arr);
-	//if (!(*line))
-	//	return (-1);
 	new = ft_strdup(eol + 1);
 	if (!new)
 		return (-1);
-	//free(*arr);
+	free(*arr);
 	*arr = new;
-	//if((*arr + res) != NULL)
-	//*arr = *arr + res;
-	//if (*arr == NULL)
-	//	return (-1);
 	return (1);
 }
 
 static int	ft_fill(char **line, char **arr, const int fd, char *eol)
 {
 	ssize_t	ret;
-	//char	*head[FD_SIZE];
 
-	//head[fd] = arr[fd];
 	while (!eol)
 	{
-	//	ft_joinandfree(line, arr);
-	//	ft_malloc_buffer(&arr[fd], &head[fd]);
-	//	head[fd] = arr[fd];
 		if (ft_joinandfree(line, arr) == -1 || ft_malloc_buffer(arr) == -1)
 			return (-1);
 		ret = read(fd, *arr, BUFF_SIZE);
@@ -75,14 +63,13 @@ static int	ft_fill(char **line, char **arr, const int fd, char *eol)
 			return (-1);
 		if (ret == 0)
 		{
-			if (ft_strlen(*line))
+			if (ft_strlen(*line) > 0)
 			{
 				**arr = '\0';
 				return (1);
 			}
-			//if (*arr)
-			ft_strdel(arr); /* this break function */
-			ft_strdel(line); /* if enabled there is (NULL) in end */
+			ft_strdel(arr);
+			ft_strdel(line);
 			return (0);
 		}
 		(*arr)[ret] = '\0';
@@ -98,16 +85,9 @@ int	get_next_line(const int fd, char **line)
 
 	if (fd < 0 || fd > FD_SIZE || !line || BUFF_SIZE <= 0)
 		return (-1);
-//	if (!arr[fd])
-//		return (-1);
-
-	//*line = NULL;
-	//if (line && *line)
-	*line = ft_strnew(0);
-//	if (*line == NULL)
-//		return (-1);
 	if (!arr[fd])
 		ft_malloc_buffer(&arr[fd]);
+	*line = ft_strnew(0);
 	eol = ft_strchr(arr[fd], '\n');
 	return (ft_fill(line, &arr[fd], fd, eol));
 }
